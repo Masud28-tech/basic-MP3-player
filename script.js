1923ba603419e72ce3f9dc259eed1abb7a8a921e
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2024-04-20 13:09:20 +0000

    move delete function above.

--- script.js
+++ script.js
@@ -146,6 +146,35 @@ const pauseSong = () => {
   audio.pause();
 }
 
+const deleteSong = (id) => {
+  if(userData?.currentSong?.id === id){
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+  }
+  userData.songs = userData?.songs.filter((song) => song.id !== id);
+  renderSongs(userData?.songs);
+  highlightCurrentSong();
+  setPlayButtonAccessibleText();
+
+  if(userData?.songs.length === 0){
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener('click', () => {
+      userData.songs = [...allSongs];
+      renderSongs(sortSongs());
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+  }
+}
+
 pauseButton.addEventListener('click', pauseSong);
 
 const getCurrentSongIndex = () => {
@@ -214,35 +243,6 @@ const shuffle = () => {
   setPlayButtonAccessibleText();
 }
 shuffleButton.addEventListener('click', shuffle);
-
-const deleteSong = (id) => {
-    if(userData?.currentSong?.id === id){
-      userData.currentSong = null;
-      userData.songCurrentTime = 0;
-      pauseSong();
-      setPlayerDisplay();
-    }
-    userData.songs = userData?.songs.filter((song) => song.id !== id);
-    renderSongs(userData?.songs);
-    highlightCurrentSong();
-    setPlayButtonAccessibleText();
-
-    if(userData?.songs.length === 0){
-      const resetButton = document.createElement("button");
-      const resetText = document.createTextNode("Reset Playlist");
-      resetButton.id = "reset";
-      resetButton.ariaLabel = "Reset playlist";
-      resetButton.appendChild(resetText);
-      playlistSongs.appendChild(resetButton);
-
-      resetButton.addEventListener('click', () => {
-        userData.songs = [...allSongs];
-        renderSongs(sortSongs());
-        setPlayButtonAccessibleText();
-        resetButton.remove();
-      });
-    }
-}
 
 audio.addEventListener('ended', () => {
   const currentSongIndex = getCurrentSongIndex();
